f23d65543f0027cc4992f29b33eae9f64cb76e92
Mahmoud Nabhan <niypoo@gmail.com>
2023-10-18 10:53:54 +0300

    Update settings.controller.dart

--- lib/settings/settings.controller.dart
+++ lib/settings/settings.controller.dart
@@ -15,35 +15,37 @@ class SettingsController extends GetxController {
 
   // show options menu
   Future<void> onContactUsMenuShow() async {
-    // show menu option
-    final String? payload = await ActionSheetHelper.show(
-      title: 'Contact us'.tr,
-      subTitle:
-          'Facing a issue and need help! have an idea please feel free.'.tr,
-      options: [
-        ActionSheetOption(
-          title: 'Problem Found'.tr,
-          value: 'ISSUE',
-          leading: const Icon(UniconsLine.bug),
-        ),
-        ActionSheetOption(
-          title: 'Feature Missing'.tr,
-          value: 'FEATURE',
-          leading: const Icon(UniconsLine.star_half_alt),
-        ),
-      ],
-      height: null,
-    );
+    try {
+      // show menu option
+      final String? payload = await ActionSheetHelper.show(
+        title: 'Contact us'.tr,
+        subTitle:
+            'Facing a issue and need help! have an idea please feel free.'.tr,
+        options: [
+          ActionSheetOption(
+            title: 'Problem Found'.tr,
+            value: 'ISSUE',
+            leading: const Icon(UniconsLine.bug),
+          ),
+          ActionSheetOption(
+            title: 'Feature Missing'.tr,
+            value: 'FEATURE',
+            leading: const Icon(UniconsLine.star_half_alt),
+          ),
+        ],
+        height: null,
+      );
 
-    print('payload $payload');
-    
-    // skip
-    if (payload == null) return;
+      // skip
+      if (payload == null) return;
 
-    LaunchUrlHelper.toEmail(
-      email: AppConfigService.to.supportEmail,
-      subject: "${AppConfigService.to.appName}-$payload".tr,
-    );
+      LaunchUrlHelper.toEmail(
+        email: AppConfigService.to.supportEmail,
+        subject: "${AppConfigService.to.appName}-$payload".tr,
+      );
+    } catch (e) {
+      print('ERROR $e');
+    }
   }
 
   // show alert guest
